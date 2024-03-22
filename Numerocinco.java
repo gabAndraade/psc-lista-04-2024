1c23fc8151ed0265ea318f57b141edf26a0899e8
Gabriel Andrade <160685894+gabAndraade@users.noreply.github.com>
2024-03-22 17:09:41 -0300

    changed code

--- Numerocinco.java
+++ Numerocinco.java
@@ -3,69 +3,36 @@ import java.util.Scanner;
 public class Numerocinco {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int paisB;
         int paisA;
+        int paisB;
         double taxaA;
         double taxaB;
-        String reset;
+        String res;
 
-
-        System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país A: ");
-            paisA = scanner.nextInt();
-            taxaA = scanner.nextDouble();
-        System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país B: ");
-            paisB = scanner.nextInt();
-            taxaB = scanner.nextDouble();
-        while(paisA < 0 || paisB < 0 || taxaA < 0 || taxaB < 0){
-                System.out.println("Há um valor inválido, tente novamente.");
-                System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país A: ");
-                    paisA = scanner.nextInt();
-                    taxaA = scanner.nextDouble();
-                System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país B: ");
-                    paisB = scanner.nextInt();
-                    taxaB = scanner.nextDouble();
-        }
-        
-        int qntdAnos = 0;
-        while(paisA<=paisB){ 
-            paisA *= taxaA;
-            paisB *= taxaB;
-            System.out.println(paisA);
-            qntdAnos++;          
-        }
-        System.out.println("A quantidade de anos possiveis para o paísA passar o paísB é: " + qntdAnos + "anos");
-
-        
-        System.out.println("Digite R para fazer a repetição do programa.Caso queira sair digite Q.");
+        do{
+        System.out.println("Informe a população e a taxa de crescimento inicial do país A.");
+        paisA = scanner.nextInt();
+        taxaA = scanner.nextDouble()/100;
+        System.out.println("Informe a população e a taxa de crescimento inicial do país B.");
+        paisB = scanner.nextInt();
+        taxaB = scanner.nextDouble()/100;
         scanner.nextLine();
-        reset = scanner.nextLine();
-        while(reset.equalsIgnoreCase("R")) {
-            if (reset.equalsIgnoreCase("Q")) {
-                System.out.println("Encerrando...");
-                break;
-                
-                
-            }
-        System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país A: ");
-            paisA = scanner.nextInt();
-            taxaA = scanner.nextDouble();
-        System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país B: ");
-            paisB = scanner.nextInt();
-            taxaB = scanner.nextDouble();
-            while(paisA < 0 || paisB < 0 || taxaA < 0 || taxaB < 0){
-                System.out.println("Há um valor inválido, tente novamente.");
-                System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país A: ");
-                    paisA = scanner.nextInt();
-                    taxaA = scanner.nextDouble();
-                System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país B: ");
-                    paisB = scanner.nextInt();
-                    taxaB = scanner.nextDouble();
-                   
-        }                                
-        System.out.println("A quantidade de anos possiveis para o paísA passar o paísB é: " + qntdAnos + "anos");            
-        }             
-        scanner.close();    
+
+        int anos = 0;
+        while(paisA <= paisB){
+        paisA += paisA * taxaA;
+        paisB += paisB * taxaB;
+        anos++;       
+        }
+        System.out.println("A quantidade de anos necessários para o país A passar o país B é: "+anos+"anos");
+        System.out.println("Digite 'R' para repetir a operação.Caso queira sair digite 'Q'.");
+        res = scanner.nextLine();
+        scanner.close();
+        if (res.equalsIgnoreCase("q")) {
+            System.out.println("Encerrando...");
+            return;               
+        }        
+        }while(res.equalsIgnoreCase("r"));
+        
+        }
     }
-    
-    
-}
