f757d4afb470d685153fb5c228e87492fed14559
Gabriel Andrade <160685894+gabAndraade@users.noreply.github.com>
2024-03-22 00:21:03 -0300

    add repeat

--- Numerocinco.java
+++ Numerocinco.java
@@ -7,6 +7,9 @@ public class Numerocinco {
         int paisA;
         double taxaA;
         double taxaB;
+        String reset;
+
+
         System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país A: ");
             paisA = scanner.nextInt();
             taxaA = scanner.nextDouble();
@@ -22,7 +25,7 @@ public class Numerocinco {
                     paisB = scanner.nextInt();
                     taxaB = scanner.nextDouble();
         }
-        scanner.close();
+        
         int qntdAnos = 0;
         while(paisA<=paisB){ 
             paisA *= taxaA;
@@ -31,6 +34,38 @@ public class Numerocinco {
             qntdAnos++;          
         }
         System.out.println("A quantidade de anos possiveis para o paísA passar o paísB é: " + qntdAnos + "anos");
+
+        
+        System.out.println("Digite R para fazer a repetição do programa.Caso queira sair digite Q.");
+        scanner.nextLine();
+        reset = scanner.nextLine();
+        while(reset.equalsIgnoreCase("R")) {
+            if (reset.equalsIgnoreCase("Q")) {
+                System.out.println("Encerrando...");
+                break;
+                
+                
+            }
+        System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país A: ");
+            paisA = scanner.nextInt();
+            taxaA = scanner.nextDouble();
+        System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país B: ");
+            paisB = scanner.nextInt();
+            taxaB = scanner.nextDouble();
+            while(paisA < 0 || paisB < 0 || taxaA < 0 || taxaB < 0){
+                System.out.println("Há um valor inválido, tente novamente.");
+                System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país A: ");
+                    paisA = scanner.nextInt();
+                    taxaA = scanner.nextDouble();
+                System.out.println("Informe quantos habitantes e a taxa de crescimento anual do país B: ");
+                    paisB = scanner.nextInt();
+                    taxaB = scanner.nextDouble();
+                   
+        }                                
+        System.out.println("A quantidade de anos possiveis para o paísA passar o paísB é: " + qntdAnos + "anos");            
+        }             
+        scanner.close();    
     }
+    
     
 }
