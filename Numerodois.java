fda0f1c0f75b29db2bf3912bcad2b18a233c64bb
Gabriel Andrade <160685894+gabAndraade@users.noreply.github.com>
2024-03-22 23:33:50 -0300

    add one more scanner

--- Numerodois.java
+++ Numerodois.java
@@ -11,6 +11,8 @@ public class Numerodois {
         senhaUsuario = scanner.nextLine();
             while(senhaUsuario.equalsIgnoreCase(nomeUsuario)){
                 System.out.println("ERRO. Sua senha não pode ser igual ao seu nome de usuario, tente novamente.");
+                System.out.println("Digite seu nome de usuário:");
+                nomeUsuario = scanner.nextLine();
                 System.out.println("Digite sua senha:");
                 senhaUsuario = scanner.nextLine();                
             }
